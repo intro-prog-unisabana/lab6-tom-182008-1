3090e194f76cf5e53813500307c5bb430ec9b2a2
Tomás Rodriguez <tomasrour@unisabana.edu.co>
2026-04-08 08:01:59 -0500

    overall

--- overall.py
+++ overall.py
@@ -1,13 +1,20 @@
 def student_averages(data):
+    if not data:
+        return {}
+        
     results = {}
     for student, assignments in data.items():
         grades = assignments.values()
+        if not grades:
+            results[student] = 0
+            continue
         avg = round(sum(grades) / len(grades))
         results[student] = avg
     return results
 
 def assignment_averages(data):
-   
+    if not data:
+        return {}
     first_student = list(data.keys())[0]
     assignment_names = data[first_student].keys()
     
@@ -17,7 +24,8 @@ def assignment_averages(data):
     for task in assignment_names:
         total_score = 0
         for student in data:
-            total_score += data[student][task]
+            # Usamos .get() con 0 por seguridad extra
+            total_score += data[student].get(task, 0)
         
         avg = round(total_score / num_students)
         results[task] = avg
