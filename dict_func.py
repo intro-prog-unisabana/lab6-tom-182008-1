c8f90c03a14326c6d362bc60bd7a0843590298be
Tomás Rodriguez <tomasrour@unisabana.edu.co>
2026-04-08 07:37:04 -0500

    dict_func

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,15 @@
 # Write your code here!
+def employee_print(employee_info):
+    info_copy = employee_info.copy()
+    
+    required_keys = ["Name", "Salary", "Role"]
+    
+    for key in required_keys:
+        value = info_copy.pop(key, "N/A")
+        print(f"{key}: {value}")
+    
+    if not info_copy:
+        print("No other info!")
+    else:
+        for key, value in info_copy.items():
+            print(f"{key}: {value}")
